419b6d59507eed0e5e44e3e5028bee6157e60b1e
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2018-12-13 22:08:24 +0300

    update task 6

--- hw4.hs
+++ hw4.hs
@@ -14,27 +14,30 @@ minMax (x:xs) = let mx = minMax xs
 
 
 -- Task4
-helper :: Eq a => [a] -> Maybe(a) -> Int -> Maybe(a)
-helper x candidate confidence =
+majorItemHelper :: Eq a => [a] -> Maybe(a) -> Int -> Maybe(a)
+majorItemHelper x candidate confidence =
     if null x
         then if confidence > 0
             then candidate
             else Nothing
         else if confidence == 0
-            then helper (tail x) (Just (head x)) 1
+            then majorItemHelper (tail x) (Just (head x)) 1
             else if candidate == Just(head x)
-                then helper (tail x) candidate (confidence + 1)
-                else helper (tail x) candidate (confidence - 1)
+                then majorItemHelper (tail x) candidate (confidence + 1)
+                else majorItemHelper (tail x) candidate (confidence - 1)
 
 majorItem :: Eq a => [a] -> Maybe(a)
-majorItem x = helper x Nothing 0
+majorItem x = majorItemHelper x Nothing 0
 -- source: https://habr.com/post/167177/
 
--- Task 6
+-- Task 6 (updated)
 lastFibNum :: Int -> Int
-lastFibNum 0 = 0
-lastFibNum 1 = 1
-lastFibNum n = ( lastFibNum(n-2) + lastFibNum(n-1) ) `mod` 10
+lastFibNum n = mod (lastFibNumHelper 0 1 n) 10
+
+lastFibNumHelper current previous n
+    | n == 0 = current
+    | n > 0 = lastFibNumHelper (current + previous) current (n - 1)
+    | n < 0 = lastFibNumHelper previous (current - previous) (n + 1)
 
 -- Task7
 isPalindrome :: String -> Bool
