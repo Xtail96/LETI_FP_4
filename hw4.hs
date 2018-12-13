12600edfe5dbc9d5cdddbee894e446fe4884eabf
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2018-12-13 21:57:59 +0300

    refactor

--- hw4.hs
+++ hw4.hs
@@ -5,29 +5,29 @@ import Data.List
 import Data.Char
 
 -- Task2
-min_max :: Ord val => [val] -> Maybe (val, val)
-min_max [] = Nothing
-min_max (x:xs) = let mx = min_max xs
+minMax :: Ord val => [val] -> Maybe (val, val)
+minMax [] = Nothing
+minMax (x:xs) = let mx = minMax xs
   in if isNothing(mx) 
     then Just (x,x) 
     else Just (min x (fst (fromJust mx)), max x (snd (fromJust mx)))
 
 
 -- Task4
-major_item :: Eq a => [a] -> Maybe(a) -> Int -> Maybe(a)
-major_item x candidate confidence =
+helper :: Eq a => [a] -> Maybe(a) -> Int -> Maybe(a)
+helper x candidate confidence =
     if null x
         then if confidence > 0
             then candidate
             else Nothing
         else if confidence == 0
-            then major_item (tail x) (Just (head x)) 1
+            then helper (tail x) (Just (head x)) 1
             else if candidate == Just(head x)
-                then major_item (tail x) candidate (confidence + 1)
-                else major_item (tail x) candidate (confidence - 1)
+                then helper (tail x) candidate (confidence + 1)
+                else helper (tail x) candidate (confidence - 1)
 
-majority :: Eq a => [a] -> Maybe(a)
-majority x = major_item x Nothing 0
+majorItem :: Eq a => [a] -> Maybe(a)
+majorItem x = helper x Nothing 0
 -- source: https://habr.com/post/167177/
 
 -- Task 6
@@ -40,10 +40,9 @@ lastFibNum n = ( lastFibNum(n-2) + lastFibNum(n-1) ) `mod` 10
 isPalindrome :: String -> Bool
 isPalindrome [] = True
 isPalindrome [x] = True
-isPalindrome (x:xs) =
-    if toUpper(x) /= toUpper(last xs)
-    then False
-    else isPalindrome(init xs)
+isPalindrome (x:xs)
+    | toUpper(x) /= toUpper(last xs) = False
+    | otherwise = isPalindrome(init xs)
 
 
 -- Additional Task
